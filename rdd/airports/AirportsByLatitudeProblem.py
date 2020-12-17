f6efaf877bbc1fef0e6469c6038360666925c3f2
korlov01 <korlov01@dcs.bbk.ac.uk>
2020-12-17 09:42:31 +0000

    started

--- rdd/airports/AirportsByLatitudeProblem.py
+++ rdd/airports/AirportsByLatitudeProblem.py
@@ -1,6 +1,24 @@
-from pyspark import SparkContext
+import sys
+sys.path.insert(0, '.')
+from pyspark import SparkContext, SparkConf
+from commons.Utils import Utils
+
+def splitComma(line: str):
+    splits = Utils.COMMA_DELIMITER.split(line)
+    return "{}, {}".format(splits[1], splits[6])
 
 if __name__ == "__main__":
+    conf = SparkConf().setAppName("airports").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+    
+    airports = sc.textFile("in/airports.text")
+    
+    airportsInUSA = airports.filter(lambda line: float(Utils.COMMA_DELIMITER.split(line)[6]) > 40)
+    
+    airportsNameAndCityNames = airportsInUSA.map(splitComma)
+    
+    airportsNameAndCityNames.saveAsTextFile("out/airports_by_latitude.text")
+    
 
     '''
     Create a Spark program to read the airport data from in/airports.text,  find all the airports whose latitude are bigger than 40.
@@ -15,3 +33,4 @@ if __name__ == "__main__":
     "Tofino", 49.082222
     ...
     '''
+    
